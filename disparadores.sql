113bde6ed9920368aab19eca52b6594bd4e7ae5e
Antonio Escuchas <antonioescuchas080@gmail.com>
2024-05-17 18:12:54 +0200

    Disparador ID

--- disparadores.sql
+++ disparadores.sql
@@ -1,9 +1,78 @@
+/*
+
+    DISPARADORES / FUNCIONES / PROCEDIMIENTOS
+
+*/
+
+-- Formato y activador out put servidor
 set serveroutput on;
 alter session set nls_date_format = 'DD/MM/YYYY HH24:MI:SS';
 
+-- Disparador para controlar que el DNI sea correcto
+-- En este caso usa ID en vez de DNI
+
+create or replace trigger trigger_participantes_id
+before insert 
+on participantes
+referencing new as 
+new for each row -- por cada fila insertada
+
+begin 
+    -- Bucle para verificar cuando inserta
+    if inserting then
+        -- Bucle para verificar que el ID no se null ni este vacio
+        if (:new.asociado is not null or :new.asociado not like '') then
+            -- Bucle para verificar que se inserten número y no letras
+            if (:new.asociado not like '%(^0-9)%') then 
+                -- ERROR
+                raise_application_error(-20001, 'El id no puede llevar caracteres');
+            
+            else
+                -- Bucle para verificar que el Id no existe
+                if (:new.asociado > num_max_asociado) then
+                
+                    insert into participantes (asociado, nombre, ap1, ap2, correo_electronico, telefono, tipo_via, num_via, nombre_via, bloque, escalera, planta, puerta, seccion, nombreloc, nombreprov)
+                    values (:new.asociado, :new.nombre, :new.ap1, :new.ap2, :new.correo_electronico, :new.telefono, :new.tipo_via, :new.nombre_via, :new.bloque, :new.escalera, :new.planta, :new.puerta, :new.seccion, :new.nombreloc, :new.nombreprov);
+                
+                else 
+                    -- ERROR
+                     raise_application_error(-20002, 'El id insertado ya existe');
+                
+                end if;
+            
+            end if; 
+        
+         else
+            -- ERROR
+            raise_application_error(-20003, 'El id no puede estar vacio');
+        
+        end if; 
+
+    end if;
+
+end;
+/
 
 
+/*
 
+    Función devuelve el id mas alto
+
+*/
+create or replace function num_max_asociado return number
+as 
+
+    n_max number;
+
+begin
+    
+    select max(to_number(asociado)) into n_max
+    from participantes;
+
+    return n_max;
+
+end;
+/
 
 
 -- Insert prueba competición
